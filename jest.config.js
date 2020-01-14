121140bcb0adf11b52423bb66c5a5efaba247546
rpenf <rpenfold@live.com>
2020-01-13 22:31:38 -0700

    added prettier

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-        "src/**/*.{ts|js}",
-        "!**/node_modules/**",
-        "!**/dist/**"
-      ],
-        "coverageThreshold": {
-        "global": {
-          "branches": 100,
-          "functions": 100,
-          "lines": 100,
-          "statements": 100
-        }
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts|js}",
+    "!**/node_modules/**",
+    "!**/dist/**"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
     }
+  }
 };
